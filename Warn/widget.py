af0617e491b4bb84950fa44bc75ce0f694314d92
hanrui <rui19921122@qq.com>
2016-06-26 17:21:51 +0800

    g

--- Warn/widget.py
+++ Warn/widget.py
@@ -1,30 +1,38 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 from functools import partial
 
-
 class Warning(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(Warning, self).__init__()
         self.parent = parent
-        self.pal = QtGui.QPalette()
-        self.pal.setColor(QtGui.QPalette.Background, QtGui.QColor(200, 253, 123, 100))
-        self.setPalette(self.pal)
         self.main_layout = QtWidgets.QVBoxLayout()
-        self.button = QtWidgets.QPushButton('add')
-        self.main_layout.addWidget(self.button)
         self.setLayout(self.main_layout)
+        self.main_layout.setAlignment(QtCore.Qt.AlignTop)
+        self.setMaximumWidth(500)
+        self.setMinimumHeight(500)
+        self.setGeometry(0, 0, 100, 100)
 
-    def add_warn(self, message):
+    def add_warn(self, message, type='warn', delay=3):
+        self.parent.show()
         label = QtWidgets.QLabel()
+        label.setAutoFillBackground(True)
+        if type == 'warn':
+            color = '#1DFFDF'
+        else:
+            color = '#1DFFDF'
+        label.setStyleSheet('background-color:{};border-radius:10px'.format(color))
         label.setText(message)
-        label.setPalette(self.pal)
         label.setAlignment(QtCore.Qt.AlignCenter)
+        label.setMaximumHeight(200)
+        label.setMinimumHeight(200)
         self.main_layout.addWidget(label)
         timer = QtCore.QTimer(self.parent)
-        timer.start(5000)
+        timer.start(delay * 1000)
         timer.timeout.connect(partial(self.remove_warn, timer=timer, label=label))
 
     def remove_warn(self, timer: QtCore.QTimer, label: QtWidgets.QLabel):
         timer.stop()
         label.setParent(None)
         label.destroy()
+        if self.main_layout.count() == 0:
+            self.parent.hide()
--- mainWindow.py
+++ mainWindow.py
@@ -14,73 +14,83 @@ from data import DataStore
 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.store = DataStore()
-        self.settings = self.store.settings
-        self.data = self.store.data
-        self.setWindowTitle("芜湖东站点名会系统")
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(r'..\resource\icon.png'),
-                       QtGui.QIcon.Normal,
-                       QtGui.QIcon.Off)
-        self.setWindowIcon(icon)
-        warn = WarnWidget.Warning(parent=self)
-        self.setCentralWidget(warn)
-        warn.add_warn('5151')
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.store = DataStore()
+		self.settings = self.store.settings
+		self.data = self.store.data
+		self.setWindowTitle("芜湖东站点名会系统")
+		icon = QtGui.QIcon()
+		icon.addPixmap(QtGui.QPixmap(r'..\resource\icon.png'),
+					   QtGui.QIcon.Normal,
+					   QtGui.QIcon.Off)
+		self.setWindowIcon(icon)
+		self.warn = WarnWidget.Warning(parent=self.dockWidget)
+		self.warn.add_warn('5151')
+        self.setup_dock_widget()
 
-    def login_commit(self, username, password, boolean):
-        response = self.session.post(url=url_resolve.parse_url('api/auth/login/'),
-                                     data={'email': '',
-                                           'password': password,
-                                           'username': username
-                                           })
-        """:type :requests.Response """
-        if response.status_code == 200:
-            _get_detail_response = self.session.get(url=url_resolve.parse_url('api/menu/get-user-detail'))
-            self.username = _get_detail_response.json().get('name')
-            self.department = _get_detail_response.json().get('department')
-            if boolean:
-                self.setCentralWidget(
-                    FigureCollectionLogic.FigureCollectionLogic(main_window=self, session=self.session))
-            else:
-                self.setCentralWidget(DisplayLogic.DisplayStarting(session=self.session, main_window=self))
-        else:
-            message = QtWidgets.QMessageBox.warning(self, "错误", str(response.json()), QtWidgets.QMessageBox.Yes)
+	def setup_dock_widget(self):
+        self.dockWidget = QtWidgets.QDockWidget(self)
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dockWidget)
+        self.dockWidget.setWidget(self.warn)
+        self.dockWidget.setFeatures(
+            QtWidgets.QDockWidget.NoDockWidgetFeatures
+        )
+        self.dockWidget.setTitleBarWidget(QtWidgets.QWidget())
 
-    def start_call_over(self):
-        response = self.session.post(url=url_resolve.parse_url('api/call_over/begin-call-over/'))
-        assert isinstance(response, requests.Response)
-        if response.status_code == 200:
-            self.call_over_data = response.json()
-            # todo 新开线程处理后台传
-            print(self.call_over_data)
-            self.setCentralWidget(DisplayMainLogic.DisplayMainLogic(mainWindow=self))
-        else:
-            message = QtWidgets.QMessageBox.warning(self, "错误", str(response.json()), QtWidgets.QMessageBox.Yes)
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1082, 731)
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+	def login_commit(self, username, password, boolean):
+		response = self.session.post(url=url_resolve.parse_url('api/auth/login/'),
+									 data={'email': '',
+										   'password': password,
+										   'username': username
+										   })
+		""":type :requests.Response """
+		if response.status_code == 200:
+			_get_detail_response = self.session.get(url=url_resolve.parse_url('api/menu/get-user-detail'))
+			self.username = _get_detail_response.json().get('name')
+			self.department = _get_detail_response.json().get('department')
+			if boolean:
+				self.setCentralWidget(
+					FigureCollectionLogic.FigureCollectionLogic(main_window=self, session=self.session))
+			else:
+				self.setCentralWidget(DisplayLogic.DisplayStarting(session=self.session, main_window=self))
+		else:
+			message = QtWidgets.QMessageBox.warning(self, "错误", str(response.json()), QtWidgets.QMessageBox.Yes)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
+	def start_call_over(self):
+		response = self.session.post(url=url_resolve.parse_url('api/call_over/begin-call-over/'))
+		assert isinstance(response, requests.Response)
+		if response.status_code == 200:
+			self.call_over_data = response.json()
+			# todo 新开线程处理后台传
+			print(self.call_over_data)
+			self.setCentralWidget(DisplayMainLogic.DisplayMainLogic(mainWindow=self))
+		else:
+			message = QtWidgets.QMessageBox.warning(self, "错误", str(response.json()), QtWidgets.QMessageBox.Yes)
 
-    def closeEvent(self, event, *args, **kwargs):
-        if self.can_close:
-            event.accept()
-        else:
-            QtWidgets.QMessageBox.warning(self, "警告", "后台正在处理数据，请勿关闭")
-            event.ignore()
+	def setupUi(self, MainWindow):
+		MainWindow.setObjectName("MainWindow")
+		MainWindow.resize(1082, 731)
+		self.retranslateUi(MainWindow)
+		QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+	def retranslateUi(self, MainWindow):
+		_translate = QtCore.QCoreApplication.translate
+
+	def closeEvent(self, event, *args, **kwargs):
+		if self.can_close:
+			event.accept()
+		else:
+			QtWidgets.QMessageBox.warning(self, "警告", "后台正在处理数据，请勿关闭")
+			event.ignore()
 
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    qss = open('./OSXLite.qss').read()
-    app.setStyleSheet(qss)
-    mainWindow = MainWindow()
-    mainWindow.show()
-    mainWindow.showMaximized()
-    sys.exit(app.exec_())
+	app = QtWidgets.QApplication(sys.argv)
+	qss = open('./OSXLite.qss').read()
+	app.setStyleSheet(qss)
+	mainWindow = MainWindow()
+	mainWindow.show()
+	mainWindow.showMaximized()
+	sys.exit(app.exec_())
